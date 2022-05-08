d74190135ecf8937e408263ded853b8c10f364a0
Antonio Okoro <cheezytony1@gmail.com>
2022-05-08 15:23:37 +0100

    Used env to determine landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,27 @@
 import JoinWaitListPage from "../modules/JoinWaitListPage/JoinWaitListPage";
-import Meta from '../components/Meta';
+import Meta from "../components/Meta";
+import { LANDING_PAGE } from '../utils/constants';
+import LandingPage from '../modules/LandingPage/LandingPage';
+import PageLayout from '../components/Layout';
 
-const JoinWaitList = () => (
-  <>
-    <Meta title="Get Early Access | Mangrove" />
-    <JoinWaitListPage />
-  </>
-);
-export default JoinWaitList;
+const IndexPage = ({ landingPage }) => {
+  console.log(landingPage === 'home');
+  return landingPage === "home" ? (
+    <PageLayout>
+      <Meta title="Mangrove" />
+      <LandingPage />
+    </PageLayout>
+  ) : (
+    <>
+      <Meta title="Get Early Access | Mangrove" />
+      <JoinWaitListPage />
+    </>
+  );
+};
+
+export const getServerSideProps = () => {
+  const landingPage = LANDING_PAGE;
+  return { props: { landingPage } };
+};
+
+export default IndexPage;
